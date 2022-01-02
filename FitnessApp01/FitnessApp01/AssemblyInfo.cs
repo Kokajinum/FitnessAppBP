ac31bd81fd9d7328c5c9214d341658fb09ee266d
Kokajinum <petulikoz@gmail.com>
2022-01-03 00:22:18 +0100

    celková úprava vzhledu

--- FitnessApp01/FitnessApp01/AssemblyInfo.cs
+++ FitnessApp01/FitnessApp01/AssemblyInfo.cs
@@ -6,3 +6,5 @@ using Xamarin.Forms.Xaml;
 [assembly: ExportFont("fa-regular-400.otf", Alias = "FAR")]
 [assembly: ExportFont("fa-solid-900.otf", Alias ="FAS")]
 [assembly: ExportFont("fa-brands-regular.otf", Alias ="FAB")]
+[assembly: ExportFont("Roboto-Regular.ttf", Alias ="ROR")]
+[assembly: ExportFont("Roboto-Bold.ttf", Alias ="ROB")]
--- FitnessApp01/FitnessApp01/Views/LoginPage.xaml.cs
+++ FitnessApp01/FitnessApp01/Views/LoginPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using FitnessApp01.Services;
-
+﻿using FitnessApp01.Resx;
+using FitnessApp01.Services;
+using System;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,11 +18,50 @@ namespace FitnessApp01.Views
         protected override async void OnAppearing()
         {
             base.OnAppearing();
+            
             //await AuthBase.SignOut();
+            // pokud je zařízení offline a má platný token, necháme ho projít do diary
             if (AuthBase.IsAuthenticated())
             {
                 await Shell.Current.GoToAsync("//main-content");
             }
+            else
+            {
+                // pokud nemá platný token, tak uživatele upozorníme na chybějící internet
+                // zároven subscribujeme event, který se vyvolá při změně v připojení 
+                Connectivity.ConnectivityChanged += ConnectivityChanged;
+                if (!IsInternetAvailable())
+                {
+                    await DisplayAlert(AppResources.Error, AppResources.InternetRequired, "Ok");
+                }
+            }
+        }
+
+        private async void ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            if (IsInternetAvailable())
+            {
+                if (AuthBase.IsAuthenticated())
+                {
+                    await Shell.Current.GoToAsync("//main-content");
+                    Connectivity.ConnectivityChanged -= ConnectivityChanged;
+                }
+                else
+                {
+                    await DisplayAlert(AppResources.Change, AppResources.InternetAvailable, "Ok");
+                }
+            }
+            else
+            {
+                await DisplayAlert(AppResources.Error, AppResources.InternetRequired, "Ok");
+            }
+        }
+
+
+
+        private bool IsInternetAvailable()
+        {
+            return Connectivity.NetworkAccess == NetworkAccess.Internet;
         }
     }
 }
