43d95e1084f712ca9f1114bd4af81581ded6e088
Kokajinum <petulikoz@gmail.com>
2022-01-03 00:23:01 +0100

    pro Android vytvořen splash screen

--- FitnessApp01/FitnessApp01.Android/MainActivity.cs
+++ FitnessApp01/FitnessApp01.Android/MainActivity.cs
@@ -7,7 +7,9 @@ using Android.OS;
 
 namespace FitnessApp01.Droid
 {
-    [Activity(Label = "Fitness App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait )]
+    [Activity(Label = "Fitness App", MainLauncher = false, Icon = "@drawable/ic_launcher_foreground", Theme = "@style/MainTheme", 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | 
+        ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -17,6 +19,9 @@ namespace FitnessApp01.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            //TabLayoutResource = Resource.Layout.Tabbar;
+            //ToolbarResource = Resource.Layout.Toolbar;
+            //base.SetTheme(Resource.Style.MainTheme);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
---
+++ FitnessApp01/FitnessApp01.Android/SplashActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp01.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", Icon = "@drawable/ic_launcher_foreground", MainLauncher = true, NoHistory = true, 
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | 
+        ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashActivity : AppCompatActivity
+    {
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            //var intent = new Intent(this, typeof(MainActivity));
+            //if (Intent.Extras != null)
+            //{
+            //    intent.PutExtras(intent.Extras);
+            //}
+            //StartActivity(intent);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnBackPressed() { }
+    }
+}
