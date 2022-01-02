febfb8944160ee9bc3cb534f025b83bb5718ff2d
Kokajinum <petulikoz@gmail.com>
2022-01-03 00:18:38 +0100

    pomocná třída pro xamarin essentials connectivity

---
+++ FitnessApp01/FitnessApp01/Helpers/Connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace FitnessApp01.Helpers
+{
+    /// <summary>
+    /// Třída zjišťuje, zda je zařízení připojeno k internetu
+    /// </summary>
+    public static class Connection
+    {
+        public static bool IsConnected
+        {
+            get { return Connectivity.NetworkAccess == NetworkAccess.Internet; }
+        }
+    }
+}
