4731b61c17fcae731f5834798343fb7655780b19
Kokajinum <petulikoz@gmail.com>
2022-01-07 23:30:57 +0100

    nastavení fontů

--- FitnessApp01/FitnessApp01/App.xaml.cs
+++ FitnessApp01/FitnessApp01/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using FitnessApp01.Interfaces;
+﻿using FitnessApp01.Helpers;
+using FitnessApp01.Interfaces;
 using FitnessApp01.Services;
+using FitnessApp01.Views;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace FitnessApp01
@@ -10,15 +13,38 @@ namespace FitnessApp01
         public App()
         {
             InitializeComponent();
-            //DependencyService.Register<IDatabase,FirestoreBase>();
 
-            MainPage = new AppShell();
-            //MainPage = new LoginPage();
+            Connection.SubscribeForChanges();
+            MainPage = new AppShell();           
         }
 
         protected override void OnStart()
         {
+            SetFonts();
+        }
 
+        /// <summary>
+        /// úprava fontů pro rozlišení FHD a menší
+        /// </summary>
+        private void SetFonts()
+        {
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            var width = mainDisplayInfo.Width;
+            var height = mainDisplayInfo.Height;
+            if (width <= 1080 && height <= 1920)
+            {
+                //funguje v případě, že dodržujeme pravidlo, že 1. je setter FontSizu, jinak použít foreach
+                SettingsLabel.Setters[0].Value = 15;
+                MacronutrientsLabel.Setters[0].Value = 13;
+                AddFoodEntry.Setters[0].Value = 15;
+                //foreach (var i in SettingsLabel.Setters)
+                //{
+                //    if (i.Property.PropertyName == "FontSize")
+                //    {
+                //        i.Value = 15;
+                //    }
+                //}
+            }
         }
 
         protected override void OnSleep()
