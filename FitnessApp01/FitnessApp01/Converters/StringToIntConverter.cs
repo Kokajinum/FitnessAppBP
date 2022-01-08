32f7873f97b723ea9e9abee7c1083185f8e61e9c
Kokajinum <petulikoz@gmail.com>
2022-01-08 20:38:41 +0100

    Nový converter pro převod string vstupu na int

---
+++ FitnessApp01/FitnessApp01/Converters/StringToIntConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FitnessApp01.Converters
+{
+    public class StringToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)value == "" || value == null)
+            {
+                return 0;
+            }
+            int.TryParse((string)value, out int num);
+            return num;
+        }
+    }
+}
