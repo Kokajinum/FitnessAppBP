4c30ce45f2dd4533c84b260efaa2652e24d80e5f
Kokajinum <petulikoz@gmail.com>
2022-01-10 22:42:56 +0100

    nový interface pro dependency service, změna status bar barvy

---
+++ FitnessApp01/FitnessApp01/Interfaces/IStatusBar.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessApp01.Interfaces
+{
+    public interface IStatusBar
+    {
+        /// <summary>
+        /// Nastaví barvu status baru
+        /// </summary>
+        /// <param name="hexColor">např. "#FFFFFF"</param>
+        void SetStatusBarColor(string hexColor);
+    }
+}
