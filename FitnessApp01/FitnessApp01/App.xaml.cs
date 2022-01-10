710801ac8bcfdca67b0d0e6d0eafdfe3342c6233
Kokajinum <petulikoz@gmail.com>
2022-01-10 22:43:58 +0100

    logika pro status bar

--- FitnessApp01/FitnessApp01/App.xaml.cs
+++ FitnessApp01/FitnessApp01/App.xaml.cs
@@ -2,6 +2,7 @@
 using FitnessApp01.Interfaces;
 using FitnessApp01.Services;
 using FitnessApp01.Views;
+using System.Linq;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -15,24 +16,42 @@ namespace FitnessApp01
             InitializeComponent();
 
             Connection.SubscribeForChanges();
-            MainPage = new AppShell();           
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()
         {
-            SetFonts();
+            SetFontSize();
+            SetStatusBarColor();
+        }
+
+        private void SetStatusBarColor()
+        {
+            var activity = DependencyService.Get<IStatusBar>();
+            OSAppTheme currentTheme = Application.Current.RequestedTheme;
+            if ((currentTheme == OSAppTheme.Dark) || (currentTheme == OSAppTheme.Unspecified))
+            {
+                //var color = Application.Current.Resources
+                //    .Where(x => x.Key.Equals("TabbarColor")).FirstOrDefault().Value;
+                activity.SetStatusBarColor("#00171F");
+            }
+            else
+            {
+                activity.SetStatusBarColor("#3E92CC");
+            }
         }
 
         /// <summary>
         /// úprava fontů pro rozlišení FHD a menší
         /// </summary>
-        private void SetFonts()
+        private void SetFontSize()
         {
             var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
             var width = mainDisplayInfo.Width;
             var height = mainDisplayInfo.Height;
             if (width <= 1080 && height <= 1920)
             {
+                //úprava stylů v App.xaml
                 //funguje v případě, že dodržujeme pravidlo, že 1. je setter FontSizu, jinak použít foreach
                 SettingsLabel.Setters[0].Value = 15;
                 MacronutrientsLabel.Setters[0].Value = 13;
