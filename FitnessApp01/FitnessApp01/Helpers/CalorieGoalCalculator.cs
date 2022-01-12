c9c73c8b6555fa23dfbaeac88cf554a28e50e24e
Kokajinum <petulikoz@gmail.com>
2022-01-12 19:18:21 +0100

    komentáře

--- FitnessApp01/FitnessApp01/Helpers/CalorieGoalCalculator.cs
+++ FitnessApp01/FitnessApp01/Helpers/CalorieGoalCalculator.cs
@@ -9,32 +9,40 @@ namespace FitnessApp01.Helpers
     {
         //The Mifflin St Jeor equation
         //zdroj: https://blog.nasm.org/nutrition/resting-metabolic-rate-how-to-calculate-and-improve-yours
+        //konstanty pro úroveň aktivity (ActivityDB)
+        //zdroj: https://www.healthline.com/health/what-is-basal-metabolic-rate#How-many-calories-you-need-everyday
         public static int Calculate(RegistrationSettings rs)
         {
-            double speedConstant = 0;
-            int s = rs.GenderDB == "male" ? 5 : -161;
+            int genderConstant = rs.GenderDB == "male" ? 5 : -161;
             double result = ((10 * rs.WeightDB)
                             + (6.25 * rs.HeightDB)
-                            - (5 * rs.AgeDB) + s) * rs.ActivityDB;
+                            - (5 * rs.AgeDB) + genderConstant) * rs.ActivityDB;
 
+            double speedConstant = 1;
+            //Pokud uživatel mění DesiredWeightDB nebo WeightDB, může dojít k změně cíle
+            //hubnutí
             if (rs.DesiredWeightDB < rs.WeightDB)
             {
                 rs.GoalDB = 1;
+                //zrychlit nabírání/hubnutí lze o 0.1(10%), 0.2(20%), 0.3(30%)
                 speedConstant = 1 - rs.GoalSpeed;
-            } 
+            }
+            //nabírání
             else if (rs.DesiredWeightDB > rs.WeightDB)
             {
                 rs.GoalDB = 2;
                 speedConstant = 1 + rs.GoalSpeed + 0.05;
             }
+            //udržení
             else
             {
                 rs.GoalDB = 3;
-                speedConstant = 1;
+                //u udržení zrychlení neexistuje
             }
 
             result *= speedConstant;
-            return (int)result;
+            int actualRes = Convert.ToInt32(result);
+            return actualRes;
         }
     }
 }
